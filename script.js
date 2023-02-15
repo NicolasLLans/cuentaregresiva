7424913a8a76ac275262f10af96eaf20d6abc41b
NicolasLLans <94724163+NicolasLLans@users.noreply.github.com>
2023-02-15 14:42:45 -0300

    aplique estilos a body y al contenedor

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ function updateCountdown() {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  document.getElementById("days").innerHTML = days + "d ";
-  document.getElementById("hours").innerHTML = hours + "h ";
-  document.getElementById("minutes").innerHTML = minutes + "m ";
-  document.getElementById("seconds").innerHTML = seconds + "s ";
+  document.getElementById("days").innerHTML = days.toString().padStart(2, "0");
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, "0");
 }
 
 setInterval(updateCountdown, 1000);
